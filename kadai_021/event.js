37d80acbdb56c8497688f1e549e9a00f5556e771
toda0514haruka <haruka.toda0514@gmail.com>
2024-07-06 11:27:39 +0900

    JS課題21

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+// btn idを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// text idを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+btn.addEventListener('click', ()=>{
+  setTimeout(()=> {
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+  });
